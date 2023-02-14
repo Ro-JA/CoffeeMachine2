882517f9c28a5b6e86c6f5ca28cf624c11f3fd79
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-14 17:40:27 +0900

    new day

--- Topics/For loop and ranges/The longest sequence/src/Main.kt
+++ Topics/For loop and ranges/The longest sequence/src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    // write your code here
+    var l = mutableListOf<Int>()
+    var min = 1
+    var max = 0
+    var m = mutableListOf<Int>()
+    val n = readLine()!!.toInt()
+    repeat(n) {
+        val i = readLine()!!.toInt()
+        l.add(i)
+    }
+    for (i in l) {
+        if (i >= min) {
+            max += 1
+            min = i
+        } else {
+            m.add(max)
+        }
+    }
+    println(m.maxOrNull())
 }
