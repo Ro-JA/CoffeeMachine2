e417577d33a69e501341a2e71c6d84c6deeb3211
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-20 00:42:06 +0900

    correct

--- Topics/Enum/Danger level/src/Main.kt
+++ Topics/Enum/Danger level/src/Main.kt
@@ -1,5 +1,7 @@
-enum class DangerLevel {
-    HIGH,
-    MEDIUM,
-    LOW
+enum class DangerLevel(private val level: Int) {
+    HIGH(3),
+    MEDIUM(2),
+    LOW(1);
+
+    fun getLevel() = level
 }
