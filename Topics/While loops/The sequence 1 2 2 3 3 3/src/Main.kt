171b97d9f7e568560aaa2ae7f26ed063bdc36aa6
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-23 13:15:59 +0900

    new day

--- Topics/While loops/The sequence 1 2 2 3 3 3/src/Main.kt
+++ Topics/While loops/The sequence 1 2 2 3 3 3/src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    // put your code here
+    var a = 1
+    var b = 1
+    val n = readLine()!!.toInt()
+    repeat(n) {
+        while (a <= b) {
+            a++
+            b++
+           print(b) 
+        }
+    }
 }
