82f75e195c06de308b7cbdfd7961239faadefc98
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-25 00:56:45 +0900

    correct

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -1,23 +1,11 @@
 // complete this function, add default values
 
+fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false){
 
-fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false) {
-    var price = 0
-
-    val priceNewCar = 20000
-
-    val oldPrice = old * 2000
-
-    val kilometersPrice = if (kilometers > 10000) kilometers / 10000 * 200 else 0
-
-    val automaticPrice = if (automatic) 1500 else 0
-
-    if (maximumSpeed < 120) {
-        price = priceNewCar - oldPrice - kilometersPrice - automaticPrice - ((120 - maximumSpeed) * 100)
-    } else {
-        price = priceNewCar - oldPrice - kilometersPrice - automaticPrice + ((maximumSpeed - 120) * 100)
-    }
-
+    var price = 20000
+    price -= old * 2000
+    price += (maximumSpeed - 120) * 100
+    price -= (kilometers / 10000) * 200
+    price = if (automatic) price + 1500 else price
     println(price)
-
 }
