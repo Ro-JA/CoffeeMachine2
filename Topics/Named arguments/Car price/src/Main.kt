36c491a795deb2c98b1d9235e448b04ac1e37513
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-24 23:12:19 +0900

    new day

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -1,4 +1,17 @@
 // complete this function, add default values
-fun carPrice(old: Int, kilometers, maximumSpeed: Int, automatic: Boolean){
-
+fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false) {
+    var price = 8000
+    if(old != 5) {
+        price -=(old - 5) * 200
+    }
+    if (kilometers != 100000) {
+        price += 100000 - kilometers / 10000 * 200
+    }
+    if (maximumSpeed != 120) {
+        price -= 120 - maximumSpeed * 100
+    }
+    if (automatic) {
+        price += 1500
+    }
+    println(price)
 }
