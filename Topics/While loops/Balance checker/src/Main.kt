ada4a1b5b9da3fd16b87f988f053aca6539c3166
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-25 22:32:52 +0900

    correct

--- Topics/While loops/Balance checker/src/Main.kt
+++ Topics/While loops/Balance checker/src/Main.kt
@@ -3,8 +3,22 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
     var balance = readln().toInt()
+    var prise = 0
     while (scanner.hasNextInt()) {
-        // TODO
+        val next = scanner.nextInt()
+        prise = next
+        if (balance >= prise) {
+            balance -= prise
+            if (!scanner.hasNextInt()) {
+                println("Thank you for choosing us to manage your account! Your balance is $balance.")
+            }
+        } else {
+            println(
+                "Error, insufficient funds for the purchase. Your balance is $balance, " +
+                        "but you need $prise."
+            )
+            break
+        }
     }
-    // TODO
+
 }
