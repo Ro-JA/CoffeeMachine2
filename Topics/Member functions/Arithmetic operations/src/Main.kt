51bf14d14f68b1de987e2fa647ab8da34df73295
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-26 18:18:57 +0900

    new day

--- Topics/Member functions/Arithmetic operations/src/Main.kt
+++ Topics/Member functions/Arithmetic operations/src/Main.kt
@@ -1,1 +1,9 @@
-// write your class here
+import kotlin.math.abs
+
+class ArithmeticOperation(var x: Int, var y: Int) {
+    fun addition(): Int = x + y
+    fun subtraction() : Int = abs(x - y)
+    fun  multiplication() : Int = x * y
+    fun division() : Int = x / y
+}
+
