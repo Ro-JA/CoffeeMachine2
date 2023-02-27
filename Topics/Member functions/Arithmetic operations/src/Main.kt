c09f2dea36a2c554e368c8f94efa2a6f01a49c48
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-27 21:55:39 +0900

    new day

--- Topics/Member functions/Arithmetic operations/src/Main.kt
+++ Topics/Member functions/Arithmetic operations/src/Main.kt
@@ -1,9 +1,8 @@
 import kotlin.math.abs
 
-class ArithmeticOperations(val x: Int, val y: Int) {
+class ArithmeticOperations(private val x: Int, private val y: Int) {
     fun addition(): Int = x + y
-    fun subtraction() : Int = abs(x - y)
-    fun  multiplication() : Int = x * y
-    fun division() : Int = x / y
+    fun subtraction(): Int = abs(x - y)
+    fun multiplication(): Int = x * y
+    fun division(): Int = x / y
 }
-
