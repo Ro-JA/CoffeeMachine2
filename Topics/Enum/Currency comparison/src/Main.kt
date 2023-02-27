d47a317076727533085efc73ae80bcf91b7f1066
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-28 01:03:11 +0900

    Correct

--- Topics/Enum/Currency comparison/src/Main.kt
+++ Topics/Enum/Currency comparison/src/Main.kt
@@ -1,4 +1,33 @@
+import java.util.Currency
+import java.util.Scanner
+
 fun main() {
-    // put your code here
+    val scanner = Scanner(System.`in`)
+    val countryFirst = scanner.next()
+    val countrySecond = scanner.next()
+
+
+    try {
+        println(CurrencyCountry.valueOf(countryFirst).currency
+                == CurrencyCountry.valueOf(countrySecond).currency)
+    } catch (e: IllegalArgumentException) {
+        println(false)
+    }
+
+}
+
+enum class CurrencyCountry(val country: String, val currency: String) {
+    Germany("Germany", "Euro"),
+    Mali("Mali", "CFA franc"),
+    Dominica("Dominica", "Eastern Caribbean dollar"),
+    Canada("Canada", "Canadian dollar"),
+    Spain("Spain", "Euro"),
+    Australia("Australia", "Australian dollar"),
+    Brazil("Brazil", "Brazilian real"),
+    Senegal("Senegal", "CFA franc"),
+    France("France", "Euro"),
+    Grenada("Grenada", "Eastern Caribbean dollar"),
+    Kiribati("Kiribati", "Australian dollar");
+
 
 }
